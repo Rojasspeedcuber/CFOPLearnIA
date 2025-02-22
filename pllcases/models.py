67b7dcc787c18b51792e5a823cf3b18a5adc153a
Henrique Rojas <henriquerojinhas@gmail.com>
2025-02-22 14:20:25 -0300

    mais campos adicionados aos casos de pll

--- pllcases/models.py
+++ pllcases/models.py
@@ -5,4 +5,5 @@ class PLL_Cases(models.Model):
     id = models.AutoField(primary_key=True)
     cases = models.CharField(max_length=3)
     algorithm = models.CharField(max_length=100)
+    alternative_algorithm = models.CharField(max_length=100)
     setup = models.CharField(max_length=100)
